57f4c7cf7758326e11201477a9b33ba97171a7f5
oomura.yuuta <yuoomura@digitalshiftwave.co.jp>
2018-05-29 10:46:49 +0000

    topとshowがindexに遷移できた

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'top_kadais#show'
+  root to: 'top_kadais#index'
 
+  get "kadai_itiransyousais/index"
+  get "kadai_itiransyousais/show"
+  get "top_kadais/index"
+  get "top_kadais/show"
+  
   resources :kadai_itiransyousais
   resources :top_kadais
   resources :top_hnsts
